34ca599b2d8f09b1d3d7cf64d6f530e79d576d32
unknown <anusha.u2904@gmail.com>
2026-08-21 20:38:34 +0530

    stabilze the creation tests

--- pages/adminpage.py
+++ pages/adminpage.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 from pages.basepage import BasePage
+import random
 
 
 class AdminPage(BasePage):
@@ -9,13 +10,13 @@ class AdminPage(BasePage):
         self.drop_user_role = page.locator(".oxd-select-text").nth(0)
         self.drop_status = page.locator(".oxd-select-text").nth(1)
         self.input_emp_name = page.get_by_placeholder("Type for hints...")
-        self.input_username = page.locator("//label[text()='Username' and contains(@class,'oxd-input-field-required')]"
+        self.input_username = page.locator("//label[text()='Username']"
         "/parent::div/following-sibling::div//input")
         self.input_pwd = page.locator("input[type='password']").nth(0)
         self.input_confirm_pwd = page.locator("input[type='password']").nth(1)
         self.btn_save = page.get_by_role("button", name = "Save")
         self.btn_cancel = page.get_by_role("button" , name= "Cancel")
-        self.input_serach_user = page.locator("//label[text()='Username' and not(contains(@class,'oxd-input-field-required'))]"
+        self.input_serach_user = page.locator("//label[text()='Username']"
         "/parent::div/following-sibling::div//input")
         self.btn_search = page.get_by_role("button",name="Search")
         self.btn_confirm_delete = page.get_by_role("button", name="Yes, Delete")
@@ -30,26 +31,58 @@ class AdminPage(BasePage):
     
     def select_employee(self, empname):
         self.input_emp_name.fill(empname)
-        self.page.wait_for_timeout(1000)              
+                     
         dropdown = self.page.locator("[role='listbox']")
         expect(dropdown).to_be_visible()
-        dropdown.locator("div").first.click()       
+        searching = dropdown.get_by_text("Searching....", exact=True)
+        expect(searching).to_be_hidden()
+        option = dropdown.locator(".oxd-autocomplete-option").first
+        expect(option).to_be_visible()
+        option.click()
         
+    def select_role(self):
+        self.drop_user_role.click()       
+        dropdown = self.page.get_by_role("listbox")
+        expect(dropdown).to_be_visible()
+        role = random.choice(["Admin", "ESS"])
+        option = dropdown.get_by_role("option", name=role, exact=True)
+        expect(option).to_be_visible()
+        option.click()
+        expect(self.drop_user_role).to_contain_text(role)
         
-    def add_user(self,userrole,status,empname,username,pwd):
-        self.drop_user_role.click()
-        self.page.get_by_role("option", name=userrole).click()
+
+    def select_status(self):
         self.drop_status.click()
-        self.page.get_by_role("option", name=status).click()
+        dropdown = self.page.get_by_role("listbox")
+        expect(dropdown).to_be_visible()
+        status = random.choice(["Enabled", "Disabled"])
+        option = dropdown.get_by_role("option", name=status, exact=True)
+        expect(option).to_be_visible()
+        option.click()
+        expect(self.drop_status).to_contain_text(status)
+        
+   
+        
+        
+    def add_user(self,empname,username,pwd):
+        self.select_role()
+        self.select_status()
         self.select_employee(empname)
+        # expect(self.input_username).to_be_visible()
         self.input_username.fill(username)
+        expect(self.input_username).to_have_value(username)
         self.input_pwd.fill(pwd)
+        expect(self.input_pwd).to_have_value(pwd)
         self.input_confirm_pwd.fill(pwd)
+        expect(self.input_confirm_pwd).to_have_value(pwd)
+        expect(self.btn_save).to_be_enabled()
         self.btn_save.click()
            
         
     
     def search_user(self,username):
+        expect(self.input_serach_user).to_be_visible()
+        expect(self.input_serach_user).to_be_enabled()
         self.input_serach_user.fill(username)
         self.btn_search.click()
 
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -8,6 +8,7 @@ from utils.screenshot import ScreenshotUtil
 from pages.loginpage import LoginPage
 from pages.navigationpage import NavigationPage
 from pages.adminpage import AdminPage
+from utils.testdatagenerator import TestDataGenerator
 
 @pytest.mark.login
   
@@ -19,24 +20,16 @@ def test_naviage_page(logged_in_page):
      ScreenshotUtil.capture(logged_in_page,"admin_page")
 
 def test_adding_user(logged_in_page):
-     
-     users = ExcelReader.get_user_data("userdata.xlsx")
-     user = users[0]
-     userrole = user["Role"]
-     status  = user["Status"]
-     empname = user["empname"]
-     username = user["Username"]
-     pwd = user["Password"]
+     username = TestDataGenerator.random_username()
+     pwd = TestDataGenerator.random_password()
+     empname = TestDataGenerator.random_employee_prefix()
      nav = NavigationPage(logged_in_page)
      nav.navigate_to_admin()
      admin = AdminPage(logged_in_page)
 
      admin.add_userbutton()
-     admin.add_user(userrole,status,empname,username,pwd)     
-     
-     
+     admin.add_user(empname,username,pwd)  
 
-     
      expect(logged_in_page.get_by_role("heading" ,name="Admin")).to_be_visible()
      admin.search_user(username)
      ScreenshotUtil.capture(logged_in_page,f"user_create_{username}")
@@ -46,14 +39,11 @@ def test_delete_user(logged_in_page):
      nav.navigate_to_admin()
      admin = AdminPage(logged_in_page)
      admin.add_userbutton()
-     users = ExcelReader.get_user_data("userdata.xlsx")
-     user = users[1]
-     userrole = user["Role"]
-     status = user["Status"]
-     empname = user["empname"]
-     username = user["Username"]
-     pwd = user["Password"]
-     admin.add_user(userrole, status,empname,username,pwd)
+     username = TestDataGenerator.random_username()
+     pwd = TestDataGenerator.random_password()
+     empname = TestDataGenerator.random_employee_prefix()     
+     admin.add_user(empname,username,pwd)
+     admin.search_user(username)
      admin.delete_user(username)
      ScreenshotUtil.capture(logged_in_page,f"user_deleted_{username}")
 
